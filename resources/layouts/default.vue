2677314169c038887cfdcf09c305b2b6173577ff
Kirill <lirik2000@bk.ru>
2018-09-06 18:00:10 +0600

    First commit. Create reg form

---
+++ resources/layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app white>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer  app>
+      <span>&copy; 2018</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        items: [
+          { icon: 'apps', title: 'Welcome', to: '/' },
+          { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+        ],
+        title: 'Vuetify.js'
+      }
+    }
+  }
+</script>
